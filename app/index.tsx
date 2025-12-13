ce9a7abed228dc4bf6bdd2b728d2037dcaed42fd
Chaiwat-xz <chaiwat.thongbut@e-tech.ac.th>
2025-12-13 16:08:07 +0700

    new

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,49 @@
-import {View, Text, Button, StyleSheet} from "react-native";
 import { useRouter } from "expo-router";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-export default function App(){
+export default function App() {
 
     const router = useRouter()
 
-    return(
-        <View style={styles.box}>
-            <Text style={styles.textTitle}>HOME</Text>
-            <Button title="Next" onPress={() => router.navigate('/rectangle')}/>
+    return (
+        <View style={{ flex: 1 }}>
+
+            <View style={styles.box}>
+
+                <Text style={styles.textTitle}>HOME</Text>
+
+                <TouchableOpacity onPress={() => router.navigate('/rectangle')} >
+                    <Text style={styles.button}>
+                        Square area
+                    </Text>
+                </TouchableOpacity>
+                
+            </View>
+
         </View>
+
     )
 }
 
 const styles = StyleSheet.create({
-    box:{
-        flex:1,
-        backgroundColor:"#FCBBF3",
-        justifyContent:"center",
-        alignItems:"center"
+    box: {
+        flex: 1,
+        backgroundColor: "pink",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 20
     },
-    textTitle:{
-        fontSize:22,
-        fontWeight:"700",
-        color:"white"
+    textTitle: {
+        fontSize: 22,
+        fontWeight: "700",
+        color: "white"
+    },
+    button: {
+        fontSize: 15,
+        fontWeight: "600",
+        color: "black",
+        backgroundColor: "white",
+        padding: 8,
+        borderRadius: 10
     }
 })
--- app/rectangle.tsx
+++ app/rectangle.tsx
@@ -1,7 +1,8 @@
-import {View, Text, Button, StyleSheet, TextInput} from "react-native";
+import { View, Text, Button, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 import { useState } from "react";
+import { useRouter } from "expo-router";
 
-export default function Rectangle(){
+export default function Rectangle() {
     //พท.สี่เหลี่ยม = กว้าง * ยาว
     //      60  = 6 * 10
     // Cutput   = Input * Input
@@ -10,59 +11,116 @@ export default function Rectangle(){
     const [width, setWidth] = useState(0)
     const [length, setLength] = useState(0)
 
-    function rectangleCal(){
+    const router = useRouter()
+
+    function rectangleCal() {
         var result = width * length
         setArea(result)
     }
 
-    return(
-        <View style={styles.box}>
-            <Text style={styles.textTitle}>Square</Text>
-            {/*<Button title="Next" />*/}
+    return (
+        <View style={{ flex: 1 }}>
 
-            <Text>Width = {width} cm</Text>
-            <Text>Length = {length} cm</Text>
-            <Text>Square area = {area} cm²</Text>
+            <View style={styles.box1} >
 
-            <TextInput 
-                style={styles.textInput} 
-                placeholder="width" 
-                placeholderTextColor="gray"
-                value={width.toString()}
-                onChangeText={(w) => setWidth(Number(w))}
-            />
+                <Text style={styles.textTitle}>Square</Text>
+                {/*<Button title="Next" />*/}
 
-            <TextInput
-                style={styles.textInput}
-                placeholder="length"
-                placeholderTextColor="gray"
-                value={length.toString()}
-                onChangeText={(l) => setLength(Number(l))}
-            />
+                <View style={styles.box3}>
+                    <Text>Width = {width} cm</Text>
+                    <Text>Length = {length} cm</Text>
+                    <Text>Square area = {area} cm²</Text>
+                </View>
 
-            <Button title="Calculate" onPress={() => rectangleCal()} />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="width"
+                    placeholderTextColor="gray"
+                    value={width.toString()}
+                    onChangeText={(w) => setWidth(Number(w))}
+                />
+
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="length"
+                    placeholderTextColor="gray"
+                    value={length.toString()}
+                    onChangeText={(l) => setLength(Number(l))}
+                />
+
+                <TouchableOpacity onPress={() => rectangleCal()} >
+                    <Text style={styles.button1}>
+                        Calculate
+                    </Text>
+                </TouchableOpacity>
+
+            </View>
+
+            <View style={styles.box2}>
+
+                <TouchableOpacity onPress={() => router.navigate('/index')} >
+                    <Text style={styles.button2}>
+                        Home
+                    </Text>
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    box:{
-        flex:1,
-        backgroundColor:"#FCBBF3",
+    box1: {
+        flex: 2,
+        backgroundColor: "pink",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 20,
+        margin: 20,
+        borderRadius: 8,
+        marginTop: 50
+    },
+    box2: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 20,
+        margin: 20,
+        borderRadius: 8
+    },
+    box3: {
+        backgroundColor:"white",
         justifyContent:"center",
         alignItems:"center",
-        gap:20
+        gap:15
     },
-    textTitle:{
-        fontSize:22,
-        fontWeight:"700",
-        color:"white"
+    textTitle: {
+        fontSize: 22,
+        fontWeight: "700",
+        color: "white"
     },
-    textInput:{
-        borderWidth:2,
-        borderColor:"white",
-        width:"80%",
-        borderRadius:20,
-        textAlign:"center"
+    textInput: {
+        borderWidth: 2,
+        borderColor: "white",
+        width: "80%",
+        borderRadius: 20,
+        textAlign: "center"
+    },
+    button1: {
+        fontSize: 15,
+        fontWeight: "600",
+        color: "black",
+        backgroundColor: "white",
+        padding: 8,
+        borderRadius: 10
+    },
+    button2: {
+        fontSize: 15,
+        fontWeight: "600",
+        color: "white",
+        backgroundColor: "pink",
+        padding: 8,
+        borderRadius: 10
     }
 })
